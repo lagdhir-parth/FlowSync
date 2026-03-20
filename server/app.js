8850fc36958e9108eacab76a71756608c6b2df70
lagdhir_parth <parthlagdhir2007@gmail.com>
2026-03-20 13:22:42 +0530

    fix: update axios base URL and enable rate limiting middleware

--- server/app.js
+++ server/app.js
@@ -21,16 +21,15 @@ app.use(
   }),
 );
 app.use(helmet());
-// FIXME: Enable rate limiting
-// app.use(
-//   rateLimit({
-//     windowMs: 15 * 60 * 1000,
-//     max: 100,
-//     standardHeaders: true,
-//     legacyHeaders: false,
-//     message: { error: "Too many requests, please try again later." },
-//   })
-// );
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: { error: "Too many requests, please try again later." },
+  }),
+);
 app.use(cookieParser());
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "15mb" }));
